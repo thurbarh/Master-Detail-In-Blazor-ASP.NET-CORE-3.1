0d151c81e1e42eda4413c6b4629bcdfc0d46e9bb
Akintunde Toba <toba.akintunde@gmail.com>
2020-02-23 16:38:04 +0100

    update

---
+++ JSExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterDetailCRUD
+{
+    public static class JSExtension
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jSRuntime, string message)
+        {
+            return jSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+        public static ValueTask Alert(this IJSRuntime jSRuntime,string message)
+        {
+            return jSRuntime.InvokeVoidAsync("alert", message);
+        }
+    }
+}
